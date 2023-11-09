1350d505a732491cd4ce0e1e07eec58e7ede95b6
Aroc <85719360+liukanshan1@users.noreply.github.com>
2023-11-09 17:09:58 +0800

    创建项目

---
+++ src/main/java/top/liukanshan/shoppingwebsite/ShoppingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package top.liukanshan.shoppingwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/liukanshan/shoppingwebsite/ShoppingWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package top.liukanshan.shoppingwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
